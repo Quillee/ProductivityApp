2cf2acd3c82bb952f7608cb430f4d9520c49ce95
Melvin Peralta <peraltamdb@gmail.com>
2019-05-24 17:17:44 -0400

    Switching to PyQt officially

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 import plotly.plotly as py
 from plotly.offline import download_plotlyjs, plot, iplot
 import cufflinks as cf
-import tkinter as tk
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
 import platform
-from productivity_app.components import menu
 if 'Windows' in platform.platform():
     from win32api import GetSystemMetrics
 
 
 def main():
-    root = tk.Tk()
-    print(GetSystemMetrics(0))
-    root.geometry(newGeometry='{width}x{height}'.format(width=int(GetSystemMetrics(0) / 2),
-                                                        height=int(GetSystemMetrics(1) / 2)))
-    root.title('Generic Productivity Tracker')
-    root.config(menu=menu.get_pa_menu(root))
+    root = QApplication([])
+    window = QWidget()
+    window.setGeometry(50, 50, 500, 300)
+    window.show()
 
-    root.mainloop()
+    sys.exit(root.exec_())
 
 
 if __name__ == '__main__':
